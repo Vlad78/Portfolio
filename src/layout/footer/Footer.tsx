931ded24d40843c46046a6fcbb8e22a2a36db198
Vlad Razvenkov <dssdom@gmail.com>
2024-02-12 00:27:55 +0100

    feat: styles to footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -6,12 +6,23 @@ import Icon from "../../components/icon/Icon";
 const Footer = () => {
   return (
     <StyledFooter>
-      <FlexWrapper align="center" justify="space-evenly">
-        <div>©2022 All rights reserved.</div>
+      <FlexWrapper align="center" justify="space-evenly" direction="column">
         <Icons>
-          <Icon iconId="linkedin" width="50px" height="50px" />
-          <Icon iconId="telegram" width="50px" height="50px" />
+          <a href="#">
+            <Icon iconId="linkedin" width="50px" height="50px" />
+          </a>
+          <a href="#">
+            <Icon iconId="telegram" width="50px" height="50px" />
+          </a>
         </Icons>
+        <Creds>
+          <a href="https://www.freepik.com/free-vector/landing-page-abstract-design-template-website-app-colorful-abstract-minimal-wave_23054242.htm#page=6&query=Abstract%20gradient%20landing%20page&position=24&from_view=search&track=ais&uuid=971260ec-c6c7-4dcb-8b64-de5ff6ad5e5a">
+            Image by GarryKillian
+          </a>{" "}
+          on Freepik
+          <br></br>
+          ©2022 All rights reserved.
+        </Creds>
       </FlexWrapper>
     </StyledFooter>
   );
@@ -20,8 +31,7 @@ const Footer = () => {
 export default Footer;
 
 const StyledFooter = styled.footer`
-  background-color: #4a4a4a;
-  color: #f8f8f8;
+  margin-top: 40px;
   min-height: 20vh;
 `;
 
@@ -31,4 +41,8 @@ const Icons = styled.div`
   > :not(:last-child) {
     margin-right: 15px;
   }
+`;
+
+const Creds = styled.div`
+  margin-top: 20px;
 `;
