dd7d810ed8f018dd87a26500033574e55dbee160
Vlad Razvenkov <dssdom@gmail.com>
2024-02-29 20:24:19 +0100

    ref: remove redundant code

--- src/components/graphics/BGIllustration-new.tsx
+++ src/components/graphics/BGIllustration-new.tsx
@@ -1,7 +1,6 @@
 import { FC, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { BGGraphicsGradient } from "../../assets/images/BG-graphics/bg-graphic-set-gradient";
-import { theme } from "../../styles/Theme";
 
 type BGIllustration = {
   iconId: string;
@@ -21,26 +20,12 @@ type BGIllustration = {
   transition?: string;
 };
 
-// const extractNumberFromHwb = (hwbString: string): number => {
-//   const numberPattern = /\d+/g;
-//   const numbers = hwbString.match(numberPattern);
-//   console.log(numbers && numbers[0]);
-//   return numbers ? parseInt(numbers[0]) : 0;
-// };
-
 export const BGIllustration: FC<BGIllustration> = (props) => {
-  // const [tick, setTick] = useState({
-  //   color1: extractNumberFromHwb("hwb(89 47% 0% / 1)"),
-  //   color2: extractNumberFromHwb("hwb(161 77% 0% / 1)"),
-  //   color3: extractNumberFromHwb("hwb(77 23% 0% / 0.8)"),
-  // });
   const [tick, setTick] = useState({
     color1: 89,
     color2: 161,
     color3: 77,
   });
-
-  // console.log("tick:", tick);
 
   const requestId = useRef<number>(0);
   const start = useRef(Date.now());
@@ -53,8 +38,6 @@ export const BGIllustration: FC<BGIllustration> = (props) => {
       setTimeout(generateFrame, 100 - elapsedTime);
       return;
     }
-
-    // console.log(elapsedTime);
 
     setTick((p) => ({ color1: p.color1 + 0.6, color2: p.color2 + 1, color3: p.color3 + 0.6 }));
 
@@ -80,7 +63,6 @@ export const BGIllustration: FC<BGIllustration> = (props) => {
         fill1={`hwb(${tick.color1} 47% 0% / 1)`}
         fill2={`hwb(${tick.color2} 77% 0% / 1)`}
         fill3={`hwb(${tick.color3} 23% 0% / 0.8)`}
-        // transition={props.transition || "1s linear"}
         filter={props.filter}
       />
     </StyledBGIllustration>
@@ -91,7 +73,5 @@ const StyledBGIllustration = styled.div<Omit<BGIllustration, "iconId">>`
   position: absolute;
   inset: ${(props) => props.inset || "0 0 0 0"};
   transform: ${(props) => props.transform || ""};
-  /* filter: ${(props) =>
-    props.filter || `drop-shadow(2px 3px 8px ${theme.colors.font}) blur(0.3px)`}; */
   z-index: -1;
 `;
